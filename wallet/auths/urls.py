2d8d18ef07b9055ba5b5a6f1aacdbf8e9cff7c62
MrLokans <trikster1911@gmail.com>
2016-04-29 23:06:02 +0300

    Code reviewing, some extra fixes. TODO and README added.

--- wallet/auths/urls.py
+++ wallet/auths/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'auths'
+# app_name = 'auths'
 
-urlpatterns = [
-    # /
-    url(r'^', views.index, name='index'),
-]
+# urlpatterns = [
+#     # /
+#     url(r'^', views.index, name='index'),
+# ]
