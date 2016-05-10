6181b3eab66214520d89aff4dc0c33a05cda40f4
Nick <arioxon@gmail.com>
2016-05-10 23:13:44 +0300

    add wallet model

--- wallet/mywallet/admin.py
+++ wallet/mywallet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Wallet, AccountStatement, Currency, DiffOperation
+
+admin.site.register(Wallet)
+admin.site.register(AccountStatement)
+admin.site.register(Currency)
+admin.site.register(DiffOperation)
